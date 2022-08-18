bb1b2a5acb950f658cc86ced34ce6b1ff891016f
Krzysztof Boduch <boduch.krzysztof@gmail.com>
2022-08-18 12:38:50 +0200

    test(unit): Equals(null) test case added

--- test/Archetypes.Quantity.Test/UnitTest.cs
+++ test/Archetypes.Quantity.Test/UnitTest.cs
@@ -5,27 +5,28 @@ namespace Archetypes.Quantity.Test;
 
 public class UnitTest
 {
-    public static IEnumerable<object[]> GetTestData()
+    public static IEnumerable<object?[]> GetTestData()
     {
         var sou1 = new SystemOfUnits("b", "ar");
         var unit = new Unit(sou1, "n", "s", "d");
-        yield return new object[] {true, unit, unit};
-        yield return new object[] {true, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "n", "s", "d")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "x", "x", "x")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "x", "s", "d")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "n", "x", "d")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "n", "s", "x")};
+        yield return new object?[] {true, unit, unit};
+        yield return new object?[] {true, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "n", "s", "d")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "x", "x", "x")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), null};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "x", "s", "d")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "n", "x", "d")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou1, "n", "s", "x")};
         var sou2 = new SystemOfUnits("f", "oo");
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "n", "s", "d")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "x", "x", "x")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "x", "s", "d")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "n", "x", "d")};
-        yield return new object[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "n", "s", "x")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "n", "s", "d")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "x", "x", "x")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "x", "s", "d")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "n", "x", "d")};
+        yield return new object?[] {false, new Unit(sou1, "n", "s", "d"), new Unit(sou2, "n", "s", "x")};
     }
 
     [Theory]
     [MemberData(nameof(GetTestData))]
-    public void TestUnitEquability(bool expectedResult, Unit a, Unit b)
+    public void TestUnitEquability(bool expectedResult, Unit a, Unit? b)
     {
         Assert.Equal(expectedResult, a.Equals(b));
         Assert.Equal(expectedResult, a == b);
