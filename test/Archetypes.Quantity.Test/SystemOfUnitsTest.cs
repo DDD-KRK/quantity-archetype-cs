b9a33b3c2b082d4671d3bfa0b6c607af489d6506
Krzysztof Boduch <boduch.krzysztof@gmail.com>
2022-08-18 12:40:41 +0200

    test(system-of-unit): Equals(null) test case added

--- test/Archetypes.Quantity.Test/SystemOfUnitsTest.cs
+++ test/Archetypes.Quantity.Test/SystemOfUnitsTest.cs
@@ -5,19 +5,20 @@ namespace Archetypes.Quantity.Test;
 
 public class SystemOfUnitsTest
 {
-    public static IEnumerable<object[]> GetTestData()
+    public static IEnumerable<object?[]> GetTestData()
     {
         var systemOfUnits = new SystemOfUnits("xa", "xb");
-        yield return new object[] {true, systemOfUnits, systemOfUnits};
-        yield return new object[] {true, new SystemOfUnits("a", "b"), new SystemOfUnits("a", "b")};
-        yield return new object[] {false, new SystemOfUnits("a", "b"), new SystemOfUnits("xa", "b")};
-        yield return new object[] {false, new SystemOfUnits("a", "b"), new SystemOfUnits("a", "xb")};
-        yield return new object[] {false, new SystemOfUnits("a", "b"), new SystemOfUnits("xa", "xb")};
+        yield return new object?[] {true, systemOfUnits, systemOfUnits};
+        yield return new object?[] {true, new SystemOfUnits("a", "b"), new SystemOfUnits("a", "b")};
+        yield return new object?[] {false, new SystemOfUnits("a", "b"), null};
+        yield return new object?[] {false, new SystemOfUnits("a", "b"), new SystemOfUnits("xa", "b")};
+        yield return new object?[] {false, new SystemOfUnits("a", "b"), new SystemOfUnits("a", "xb")};
+        yield return new object?[] {false, new SystemOfUnits("a", "b"), new SystemOfUnits("xa", "xb")};
     }
 
     [Theory]
     [MemberData(nameof(GetTestData))]
-    public void TestSystemOfUnitsEquability(bool expectedResult, SystemOfUnits a, SystemOfUnits b)
+    public void TestSystemOfUnitsEquability(bool expectedResult, SystemOfUnits a, SystemOfUnits? b)
     {
         Assert.Equal(expectedResult, a.Equals(b));
         Assert.Equal(expectedResult, a == b);
